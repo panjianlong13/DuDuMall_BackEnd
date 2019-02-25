b67117b67d850226a60de2876ba827c53bf01a3d
chensha <chensha@cn.ibm.com>
2019-02-25 11:39:44 +0800

    add antd

---
+++ app-store/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+import logo from './logo.svg';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="app-root">DuDuMall-index</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
