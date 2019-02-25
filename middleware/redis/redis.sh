3063df99f2a627a62e42768bc6ffbc6b4e5f9af5
panjianlong13 <876060733@qq.com>
2019-02-25 13:50:48 +0800

    Init Project MongoDB,Redis and ELK

---
+++ middleware/redis/redis.sh
@@ -0,0 +1,1 @@
+ docker run -p 6379:6379 --name redis -v /home/DuDuMall/redis/redis.conf:/etc/redis/redis.conf -v /home/DuDuMall/redis/data:/data -d redis:3.2 redis-server /etc/redis/redis.conf --appendonly yes
