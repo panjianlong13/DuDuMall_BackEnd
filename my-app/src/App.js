f47b676f9f6a628ee7c6d2aba0107a5b16f52a37
chensha <chensha@cn.ibm.com>
2019-02-25 09:38:29 +0800

    index page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,8 +5,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div>
+     <h1>DuDuMall-index</h1> 
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +20,7 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
       </div>
     );
   }
